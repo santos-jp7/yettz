1b1122843fc937abb5f0ca4ebfde09225b5fa184
Jean Pierre <hjepih@gmail.com>
2020-01-30 08:20:44 -0300

    fix (Dm): Reply work

--- controllers/utils/Dm.js
+++ controllers/utils/Dm.js
@@ -1,5 +1,5 @@
 async function Dm(m, Client){
-    return m.repy(`Olá, ainda não posso responder mensagens por aqui!`);
+    return m.reply(`Olá, ainda não posso responder mensagens por aqui!`);
 }
 
 module.exports = Dm;
