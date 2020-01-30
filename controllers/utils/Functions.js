c506c3912854e4a3e33ca45b02d604f2887acb75
Jean Pierre <hjepih@gmail.com>
2020-01-30 05:18:14 -0300

    feat (commands): Decide

--- controllers/utils/Functions.js
+++ controllers/utils/Functions.js
@@ -53,7 +53,7 @@ class Functions{
         const {content} = m;
         let [command, ...args] = content.toLowerCase().split(' ');
 
-        return await 'a'; 
+        return await Decide(); 
 
     }
 
--- controllers/utils/functions/Decide.js
+++ controllers/utils/functions/Decide.js
@@ -1,3 +1,17 @@
+const axios = require('axios');
+const Discord = require('discord.js');
+
 module.exports = async function(args){
-    
+    const {data} = await axios.get("https://yesno.wtf/api");
+
+    let {image, answer} = data;
+    if(answer == 'no') answer = '**NÃO**';
+    if(answer == 'yes') answer = '**SIM**';
+
+    let embed = new Discord.RichEmbed();
+    embed.setColor(0xffffff);
+    embed.setDescription(answer);
+    embed.setImage(image);
+
+    return embed;
 }
