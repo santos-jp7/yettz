0e26482cff70d66a42f692e03b201c95ec54116f
Jean Pierre <hjepih@gmail.com>
2020-01-30 06:02:27 -0300

    feat (commands): Encurtar

--- controllers/utils/Functions.js
+++ controllers/utils/Functions.js
@@ -61,7 +61,7 @@ class Functions{
         const {content} = m;
         let [command, ...args] = content.toLowerCase().split(' ');
 
-        return await 'a'; 
+        return await Encurtar(args);
 
     }
 
