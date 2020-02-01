64599121c5f184031866694153a7cf60d6095e79
Jean Pierre <hjepih@gmail.com>
2020-02-01 00:02:33 -0300

    feat (commands): Limpar

--- controllers/utils/Functions.js
+++ controllers/utils/Functions.js
@@ -82,7 +82,7 @@ class Functions{
         const {content} = m;
         let [command, ...args] = content.toLowerCase().split(' ');
 
-        return await 'a'; 
+        return await Limpar(m); 
 
     }
     
--- controllers/utils/functions/Limpar.js
+++ controllers/utils/functions/Limpar.js
@@ -1,3 +1,16 @@
-module.exports = async function(args){
-    
+module.exports = async function(m){
+    const {content} = m;
+    let [command, ...args] = content.toLowerCase().split(' ');
+
+    try{
+        args = parseInt(args);
+        if(args > 100) return "Posso apagar no máximo 100 mesagens.";
+
+        const messages = await m.channel.fetchMessages({limit: args + 1});
+        m.channel.bulkDelete(messages);
+        return "Chat limpo com sucesso.";
+    }catch(e){
+        console.log(e);
+        return "Ops... Ocorreu um erro.";
+    }
 }
