f0a1c41dc0af0ab9fa9c4ebe56dcb5d9b7924e1f
Jean Pierre <hjepih@gmail.com>
2020-01-31 23:52:29 -0300

    feat (commands): Invite

--- controllers/utils/Functions.js
+++ controllers/utils/Functions.js
@@ -74,7 +74,7 @@ class Functions{
         const {content} = m;
         let [command, ...args] = content.toLowerCase().split(' ');
 
-        return await 'a';
+        return await Invite(m);
 
     }
 
--- controllers/utils/functions/Invite.js
+++ controllers/utils/functions/Invite.js
@@ -1,3 +1,9 @@
-module.exports = async function(args){
-    
+module.exports = async function(m){
+    try{
+        const {url} = await m.channel.createInvite();
+        return url;
+    }catch(e){
+        console.log(e);
+        return "Ops... Ocorreu um erro.";
+    }
 }
