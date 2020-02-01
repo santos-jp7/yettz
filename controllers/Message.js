fded08e0c192cccdb9fd75d7b90ad2e79acec592
Jean Pierre <hjepih@gmail.com>
2020-02-01 01:15:41 -0300

    fix: Mensagem de manutenção

--- controllers/Message.js
+++ controllers/Message.js
@@ -5,7 +5,7 @@ const Functions = require('./utils/Functions');
 
 async function Message(m, Client){
     if(Client.user.id == m.author.id) return;
-    if(process.env.DEV == 'yes' && m.author.id != process.env.DEV_ID) return;
+    if(process.env.DEV == 'yes' && m.author.id != process.env.DEV_ID) return m.reply("Bot em manutenção.");
     if(m.channel.type == 'dm') return Dm(m, Client);
 
     const prefix = process.env.PREFIX;
