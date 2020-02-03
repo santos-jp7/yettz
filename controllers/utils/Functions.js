fbff5487c7f5ba3af234aa3771a69341ad92fdde
Jean Pierre <hjepih@gmail.com>
2020-02-03 09:52:37 -0300

    feat (commands): Warn

--- controllers/utils/Functions.js
+++ controllers/utils/Functions.js
@@ -185,7 +185,7 @@ class Functions{
         const {content} = m;
         let [command, ...args] = content.toLowerCase().split(' ');
 
-        return await 'a'; 
+        return await Warn(m, Client); 
 
     }
 }
--- controllers/utils/functions/Warn.js
+++ controllers/utils/functions/Warn.js
@@ -1,3 +1,12 @@
-module.exports = async function(args){
+module.exports = async function(m, Client){
+    let [command, ...args] = m.content.toLowerCase().split(' ');
+
+    args = args.join(' ');
+
+    const {members} = await m.channel.guild.fetchMembers();
+    await members.forEach((member) => {
+        if(member.user.id != Client.user.id) member.send(args);
+    })
     
+    return 'Mensagens enviadas com sucesso.';   
 }
