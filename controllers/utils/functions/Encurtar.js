b7445679e527a52058ae7269a2737039e57ebbcc
Jean Pierre <hjepih@gmail.com>
2020-02-03 08:40:05 -0300

    fix: error yt

--- controllers/utils/functions/Encurtar.js
+++ controllers/utils/functions/Encurtar.js
@@ -1,27 +1,27 @@
 const axios = require('axios');
 
-//WARNING: URL INCORRECT RETURN 403
-
 module.exports = async function(args){
     const KEY = process.env.REBRANDLY_KEY;
     if(KEY == undefined) return 'Esse comando não está habilitado.';
 
     if(args.length == 0) return 'Esse comando precisa ser comentado.';
-    if(args.length > 1) return 'Verifique seu link.';
     if(args.indexOf('http') != 0) args = 'http://'+args;
 
-    const {data} = await axios.post('https://api.rebrandly.com/v1/links',{
-        destination: args,
-        domain:{
-            fullName: 'rebrand.ly'
-        }
-    },{
-        headers:{
-            apikey: KEY
-        }
-    })
-
-    if('shortUrl' in data) return 'https://'+data.shortUrl;
-    return 'Ops... Ocorreu um erro.';
-
+    try{
+        const {data} = await axios.post('https://api.rebrandly.com/v1/links',{
+            destination: args,
+            domain:{
+                fullName: 'rebrand.ly'
+            }
+        },{
+            headers:{
+                apikey: KEY
+            }
+        })
+    
+        if('shortUrl' in data) return 'https://'+data.shortUrl;
+        return 'Ops... Ocorreu um erro.';
+    }catch(e){
+        return 'Ops... Ocorreu um erro.';
+    }
 }
